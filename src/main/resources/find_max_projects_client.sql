97ec8404342c8f5aefce08c47c60453cc305ff6a
yan <yan.dasty@gmail.com>
2023-06-22 22:24:43 +0300

    Commit 2: Added 4 completed sql

---
+++ src/main/resources/find_max_projects_client.sql
@@ -0,0 +1,12 @@
+WITH common_table_expression AS (
+    SELECT name, count(*) AS project_count
+    FROM client
+    JOIN project p on client.client_id = p.client_id
+    GROUP BY name, p.client_id
+)
+SELECT name, project_count
+FROM common_table_expression
+WHERE project_count = (
+    SELECT max(project_count)
+    FROM common_table_expression
+)
