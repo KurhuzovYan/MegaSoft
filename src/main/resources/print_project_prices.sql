d687d4621f74ca5192f759d0de359fc1394b8e9c
yan <yan.dasty@gmail.com>
2023-06-23 23:18:46 +0300

    Commit 4: Finished last task

--- src/main/resources/print_project_prices.sql
+++ src/main/resources/print_project_prices.sql
@@ -1,4 +1,4 @@
-WITH count_of_months AS (
+WITH working_duration AS (
     SELECT project_id, extract(year from finish_date) * 12 + extract(month from finish_date) -
            extract(year from start_date) * 12 - extract(month from start_date) as months
     FROM project
@@ -6,23 +6,18 @@ WITH count_of_months AS (
     ORDER BY extract(year from finish_date) * 12 + extract(month from finish_date) -
              extract(year from start_date) * 12 - extract(month from start_date) DESC
 )
-SELECT project_id, count()
-FROM project
-JOIN project_worker pw on project.project_id = pw.project_id
-GROUP BY project_id;
-
-
-
 SELECT
     p.project_id,
-    SUM(w.salary * EXTRACT(MONTH FROM AGE(p.finish_date, p.start_date))) AS project_cost
-FROM
-    project p
+    SUM(salary * months) AS project_cost
+FROM project p
         JOIN
     project_worker pw ON p.project_id = pw.project_id
         JOIN
     worker w ON pw.worker_id = w.worker_id
+        JOIN
+    working_duration wd ON p.project_id = wd.project_id
 GROUP BY
-    p.project_id;
+    p.project_id
+ORDER BY project_cost DESC;
 
 
